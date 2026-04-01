94a5805635ec4b417d9a58cda4df34aeb3d37f3a
giorgiaboscolo <128316032+giorgiaboscolo@users.noreply.github.com>
2026-04-01 23:09:25 +0200

    Update aboutMe.vue

--- src/views/presentation/sections/aboutMe.vue
+++ src/views/presentation/sections/aboutMe.vue
@@ -28,7 +28,7 @@
 
                         <!-- TEXT -->
                         <blockquote class="text-sm leading-7 xl:text-base xl:leading-8 font-light text-green_giorgia-800">
-                            <p>Sono Boscolo Giorgia e ho 30 anni. Dopo aver frequentato i primi tre anni di <i>Ingegneria Edile - Architettura</i> presso l’Università di Padova,
+                            <p>Sono Boscolo Giorgia e ho 33 anni. Dopo aver frequentato i primi tre anni di <i>Ingegneria Edile - Architettura</i> presso l’Università di Padova,
                                 perseguendo i miei grandi amori per cinema e fotografia, cambio rotta conseguendo prima il diploma di laurea di primo livello in
                                 <i>Cinema, fotografia e televisione</i> all’Accademia di Belle Arti di Bologna e poi quello di secondo livello in <i>Scenografia per
                                 il cinema e la televisione</i> all’Accademia di Brera, andando così a recuperare ciò che avevo appreso in ambito progettuale nei primi tre anni del mio percorso.
